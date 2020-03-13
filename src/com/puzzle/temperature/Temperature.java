0f4e2734ea92414e8c8ff5b7700332d0062f0d9b
monacodev <nassim.chabane@gmail.com>
2020-03-13 17:19:09 +0100

    mise en forme du tableau avec une bordure gauche et droite

--- src/com/puzzle/temperature/Temperature.java
+++ src/com/puzzle/temperature/Temperature.java
@@ -3,9 +3,9 @@ package com.puzzle.temperature;
 import java.util.Scanner;
 
 public class Temperature {
-	 /*
-     * méthode de tri d'un tableau 
-     */
+	/*
+	 * méthode de tri d'un tableau
+	 */
 	public static int[] Tri(int[] tab) {
 
 		for (int i = 0; i < tab.length - 1; i++) {
@@ -19,29 +19,38 @@ public class Temperature {
 		}
 		return tab;
 	}
-	
+
 	/*
-     * méthode de tri d'un tableau 
-     */
-	
-	public static void  display(int [] tab) {
+	 * méthode de tri d'un tableau
+	 */
+
+	public static void display(int[] tab) {
+        for (int i = 0; i < 1; i++) {
+			System.out.print("|");
+		}
+       
 		for (int i = 0; i < tab.length; i++) {
-			System.out.println(tab[i]);
-			
+			System.out.print(tab[i]);
+			System.out.print("|");
 		}
 		
+		System.out.println();
+
 	}
 
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in); 
+		Scanner in = new Scanner(System.in);
 		int n = in.nextInt(); // the number of temperatures to analyse
-		int[] tab = new int[n]; // instanciation d'un tableau de taille n qui comportera toutes les temp saisies au clavier
-		
+		int[] tab = new int[n]; // instanciation d'un tableau de taille n qui comportera toutes les temp saisies
+								// au clavier
+
 		for (int i = 0; i < tab.length; i++) {
-			int t = in.nextInt(); // températures saisies  
-			tab[i]= t;
-			
+			int t = in.nextInt(); // températures saisies
+			tab[i] = t;
+
 		}
+		
+		display(tab); 
 
 	}
 
